678d7dfa223729b99670a4d35a9238cfd666532e
alenjdev <alen@formant.io>
2022-03-24 15:45:39 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Table from "./component/Table/Table";
+import "./App.css";
+import { TableComponent } from "./component/TableComponent/index";
+
+function App() {
+  return <Table />;
+}
+
+export default App;
